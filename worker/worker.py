a7ed81b676a044eeeff0ba447d42a198ffad719d
Dmitry Chiruhin <chiruhind@gmail.com>
2026-04-08 15:33:07 +0500

    Worker больше не тянет дпо инфу на github

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,6 @@
 import yt_dlp
 import json
-import sys
+import os
 
 import re
 from datetime import datetime
@@ -9,6 +9,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data.json"
+ON_GH = os.environ.get("GITHUB_ACTIONS") == "true"
 
 ydl_flat_opts = {
     'extract_flat': True,
@@ -34,6 +35,36 @@ def extract_original_link(description):
         return match.group(1)
     return None
 
+def enrich_additional_info(url, video_url, data):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+    upload_date_str = info.get('upload_date', '')
+    if upload_date_str:
+        published_at = datetime.strptime(upload_date_str, '%Y%m%d').strftime('%Y-%m-%d')
+    else:
+        published_at = ''
+
+    data['publishedAt'] = published_at
+
+    if video_url is not None:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            video_info = ydl.extract_info(video_url, download=False)
+
+        author = (
+                video_info.get('uploader') or
+                video_info.get('channel') or
+                video_info.get('uploader_id')
+        )
+
+        data['video'] = [{
+            'url': video_url,
+            'title': video_info.get('title', 'Без названия'),
+            'author': author
+        }]
+
+        data['thumbnail'] = f"https://img.youtube.com/vi/{video_info['id']}/hqdefault.jpg"
+
 def get_stream_info(entry):
     url = f"https://www.youtube.com/watch?v={entry['id']}"
     video_url = extract_original_link(entry['description'])
@@ -49,38 +80,11 @@ def get_stream_info(entry):
         }],
         'publishedAt': ''
     }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-    upload_date_str = info.get('upload_date', '')
-    if upload_date_str:
-        published_at = datetime.strptime(upload_date_str, '%Y%m%d').strftime('%Y-%m-%d')
-    else:
-        published_at = ''
 
-    data['publishedAt'] = published_at
+    if not ON_GH:
+        enrich_additional_info(url, video_url, data)
 
-    try:
-        if video_url is not None:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                video_info = ydl.extract_info(video_url, download=False)
-
-            author = (
-                    video_info.get('uploader') or
-                    video_info.get('channel') or
-                    video_info.get('uploader_id')
-            )
-
-            data['video'] = [{
-                'url': video_url,
-                'title': video_info.get('title', 'Без названия'),
-                'author': author
-            }]
-
-            data['thumbnail'] = f"https://img.youtube.com/vi/{video_info['id']}/hqdefault.jpg"
-    except Exception as e:
-        print(f"⚠️ Не удалось извлечь видео {video_url}: {e}")
-    finally:
-        return data
+    return data
 
 def main():
     streams = get_streams_from_file()
@@ -94,20 +98,19 @@ def main():
         info = ydl.extract_info(channel_url, download=False)
 
     videos = []
-    for entry in info['entries']:
+    for entry in info['entries'][:20]:
         print(entry)
         info = get_stream_info(entry)
-        if info['publishedAt'] == '' or info['publishedAt'] <= max_date:
+        if info['publishedAt'] != '' and info['publishedAt'] <= max_date:
             break
         videos.append(info)
 
-    if len(videos) == 0:
-        sys.exit(0)
+    if len(videos) > 0:
+        existing_urls = {s["url"] for s in streams}
+        unique_new_videos = [v for v in videos if v["url"] not in existing_urls]
+        streams = unique_new_videos + streams
 
-    streams = videos + streams
-    with open(DATA_PATH, "w", encoding="utf-8") as f:
-        json.dump(streams, f, ensure_ascii=False, indent=2)
-
-    sys.exit(1)
+        with open(DATA_PATH, "w", encoding="utf-8") as f:
+            json.dump(streams, f, ensure_ascii=False, indent=2)
 
 main()
